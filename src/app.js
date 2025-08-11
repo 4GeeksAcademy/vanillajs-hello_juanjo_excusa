a6f23c167df53bf2519db00d9d65ec380aeed3f2
JuanjoVillarejo <jota_villarejo22@hotmail.com>
2025-08-11 19:18:15 +0000

    refactorizado el codigo

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-
-function generateExcuse() {
-      let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-      let action = ['ate', 'peed', 'crushed', 'broke'];
-      let what = ['my homework', 'my phone', 'the car'];
-      let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-      let randomWho = who[Math.floor(Math.random() * who.length)];
-      let randomAction = action[Math.floor(Math.random() * action.length)];
-      let randomWhat = what[Math.floor(Math.random() * what.length)];
-      let randomWhen = when[Math.floor(Math.random() * when.length)];
-
-      return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    const unElementoRandom = (cadaArray) => {
+      let resultado = cadaArray[Math.floor(Math.random() * cadaArray.length)];
+      return resultado;
     }
 
-    
-window.onload = function() {
-  //write your code here
-  document.getElementById("excuse").innerHTML = generateExcuse();
-};
+console.log(unElementoRandom(who), unElementoRandom(action), unElementoRandom(what), unElementoRandom(when));
+
+
+/*const otroElementoRandom = (array1, array2, array3, array4) => {
+      let resultado1 = array1[Math.floor(Math.random() * array1.length)];
+      let resultado2 = array2[Math.floor(Math.random() * array2.length)];
+      let resultado3 = array3[Math.floor(Math.random() * array3.length)];
+      let resultado4 = array4[Math.floor(Math.random() * array4.length)];
+      
+      console.log(`${resultado1} ${resultado2} ${resultado3} ${resultado4}`);
+      
+    }
+otroElementoRandom(who, action, when, what);*/
+
